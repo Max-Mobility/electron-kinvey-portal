c324b3bdd170d67a6a77ff714e585e9e09d588e9
William Emfinger <waemfinger@gmail.com>
2019-06-26 12:20:02 -0500

    added autoupdater back and updated readme links for downloads.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 const path = require('path');
 const {app, BrowserWindow, Menu} = require('electron');
-/// const {autoUpdater} = require('electron-updater');
+const {autoUpdater} = require('electron-updater');
 const {is} = require('electron-util');
 const unhandled = require('electron-unhandled');
 const debug = require('electron-debug');
@@ -17,14 +17,14 @@ app.setAppUserModelId('com.permobil.electron.kinvey.portal');
 
 // Uncomment this before publishing your first version.
 // It's commented out as it throws an error if there are no published versions.
-// if (!is.development) {
-// 	const FOUR_HOURS = 1000 * 60 * 60 * 4;
-// 	setInterval(() => {
-// 		autoUpdater.checkForUpdates();
-// 	}, FOUR_HOURS);
-//
-// 	autoUpdater.checkForUpdates();
-// }
+if (!is.development) {
+  const FOUR_HOURS = 1000 * 60 * 60 * 4;
+  setInterval(() => {
+    autoUpdater.checkForUpdates();
+  }, FOUR_HOURS);
+
+  autoUpdater.checkForUpdates();
+}
 
 // Prevent window from being garbage collected
 let mainWindow;
--- readme.md
+++ readme.md
@@ -11,17 +11,17 @@
 
 **macOS**
 
-[**Download**](https://github.com/user/repo/releases/latest) the `.dmg` file.
+[**Download**](https://github.com/Max-Mobility/electron-kinvey-portal/releases/latest) the `.dmg` file.
 
 **Linux**
 
-[**Download**](https://github.com/user/repo/releases/latest) the `.AppImage` or `.deb` file.
+[**Download**](https://github.com/Max-Mobility/electron-kinvey-portal/releases/latest) the `.AppImage` or `.deb` file.
 
 *The AppImage needs to be [made executable](http://discourse.appimage.org/t/how-to-make-an-appimage-executable/80) after download.*
 
 **Windows**
 
-[**Download**](https://github.com/user/repo/releases/latest) the `.exe` file.
+[**Download**](https://github.com/Max-Mobility/electron-kinvey-portal/releases/latest) the `.exe` file.
 
 
 ---
